65da34eb7ef42b56051bee1a0b5b0a19e286679c
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-11-02 10:28:15 -0500

    cambios

--- 3.Data/Context/AutomovileUnitBD.cs
+++ 3.Data/Context/AutomovileUnitBD.cs
@@ -23,7 +23,7 @@ public class AutomovileUnitBD : DbContext
         if (!optionsBuilder.IsConfigured)
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-            optionsBuilder.UseMySql("Server=sql10.freemysqlhosting.net,3306;Uid=sql10658651;Pwd=KDq4fMhYws;Database=sql10658651;", serverVersion);
+            optionsBuilder.UseMySql("Server=sql3.freemysqlhosting.net,3306;Uid=sql3658786;Pwd=nFNgSaFgGj;Database=sql3658786;", serverVersion);
         }
     }
 
